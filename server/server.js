cf9dfcf7d697829085ae35d5f71ff6efd7f6910f
sanjaykumar584 <sanjay050804@gmail.com>
2024-02-10 23:30:11 +0530

    app setup

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors"
 const app = express()
 const port = 3001
 
+// CORS
 app.use(cors({origin: true}));
 app.use((_req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -11,6 +12,11 @@ app.use((_req, res, next) => {
     next();
 });
 
+// Body Parser middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// API endpoints
 app.get("/api", (req, res) => {
     // res.send("Welcome to WhereIsMyBus");
 });
